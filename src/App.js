e7db1c67c30bb0543d61baa043f42ca9956829b8
Laura Rocha <laurarochamarchese@gmail.com>
2021-07-21 19:20:45 -0300

    app and index

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styles/Theme";
+import { GlobalStyles } from "./styles/globalStyles";
+
+import { useDarkMode } from "./customhook/useDarkMode";
+import { Country } from "./contexts/state.context";
+import Header from "./components/Header";
+
+const Homepage = React.lazy(() => import("./components/Homepage"));
+const CountryDetails = React.lazy(() => import("./components/CountryDetails"));
 
 function App() {
+  const [theme, toggleTheme] = useDarkMode("light");
+
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={themeMode}>
+        <>
+          <Suspense
+            fallback={<h2 style={{ textAlign: "center" }}>Loading...</h2>}
+          >
+            <GlobalStyles />
+            <Header theme={theme} toggleTheme={toggleTheme} />
+            <Country>
+              <Switch>
+                <Route
+                  path="/detail/:id"
+                  exact
+                  render={(routeProps) => (
+                    <CountryDetails {...routeProps} theme={theme} />
+                  )}
+                />
+                <Route
+                  path="/"
+                  render={(props) => <Homepage {...props} theme={theme} />}
+                />
+              </Switch>
+            </Country>
+          </Suspense>
+        </>
+      </ThemeProvider>
     </div>
   );
 }
